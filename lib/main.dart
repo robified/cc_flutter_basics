97853b8b9b949fe9b7ef47a18f3a08382fc43f09
Robin <daorob@gmail.com>
2020-02-16 18:31:56 -0600

    Add column with 3 buttons.

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,23 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Text('This is my default text!'),
+        body: Column(
+          children: <Widget>[
+            Text('The question!'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: null,
+            ),
+          ],
+        ),
       ),
     );
   }
